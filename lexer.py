6a818f0497c8754b11b908852caecd9b1267b694
TrimNCut <jothamdavies@icloud.com>
2024-09-02 19:13:19 +0100

    Lexer update

--- lexer.py
+++ lexer.py
@@ -3,3 +3,23 @@
         input : 1 + 1 * 5;
         output: [[INT::1, "PLUS", "INT::1", "MULTIPLY", "INT::5", "BREAK"]]
 '''
+
+class Lexer:
+    def __init__(self):
+        self.tokens = []
+        self.token = []
+        self.specials = ["+", "-", "*", "/"]
+        self.lastToken = ""
+    def tokenize(self, ln):
+        for i in ln:
+            if i == " ":
+                self.token.append(self.lastToken)
+                self.lastToken = ""
+            else:
+                self.lastToken += i
+
+        return self.token
+
+lexer = Lexer()
+
+print(lexer.tokenize("1 + 1 = 5;"))
