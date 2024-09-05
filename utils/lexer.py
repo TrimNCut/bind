610208b3e6e68a1b546cd9336b0de5138dea3cd5
TrimNCut <jothamdavies@icloud.com>
2024-09-05 19:56:35 +0100

    File Structure update

--- utils/lexer.py
+++ utils/lexer.py
@@ -4,30 +4,32 @@
         output: [[INT::1, "PLUS", "INT::1", "MULTIPLY", "INT::5", "BREAK"]]
     
     BUGS:
-        -cannot detect string
-
+        - line does not need to end with ; when assigning string variables
+        - need to work for one word strings e.g "life"
+        - switch to re.search() to find strings and other variable types
+        - add more keywords
+        - find bugs
+        - might rewrite lexer (most likely)
+        - can't detect floats :/
+        - can't tokenize without using ; at the end
+        - if new; is typed it tokenizes and invisible variable name e.g [['DECLARATION', 'STR::VARIABLE::', 'BREAK']]
+        - cannot detect keywords if ; is not use to end e.g new -> [['STR::n', 'STR::w']]
+        - cannot detect strings when only typed e.g input -> "a"
 '''
 import re
+from default import *
 
 class Lexer:
     def __init__(self):
         self.tokens = []
         self.token = []
         self.tmpToken = []
-        self.specials = ["+", "-", "*", "/", ";"]
+        self.specials = ["+", "-", "*", "/", ";", "new"]
         self.lastToken = ""
         self.chars = "abcdefghijklmnopqrstuvwxyz"
         self.nums = "1234567890"
-        self.keywords = {
-            "new": "DECLARATION",
-            "func": "FUNCTION",
-            "+": "PLUS",
-            "-": "MINUS",
-            "/": "DIVIDE",
-            "*": "MULTIPLY",
-            "=": "ASSIGNMENT",
-            ";": "BREAK",
-        }
+        self.keywords = LEX_KEYWORDS
+        
     def tokenize(self, ln):
         self.token = []
         self.tmpToken = []
@@ -66,52 +68,29 @@ class Lexer:
     
     def assignTypes(self, token):
         self.lastToken = ""
-        self.inString = False
+        self.inString = 1
         for n in range(len(token)):
             i = token[n]
             self.lastToken += i
-            if "'" in i or '"' in i:
-                if self.inString:
-                    self.lastToken+= " "+i
-                if i == "'" or i == '"' or "'" in i or '"' in i:
-                    if self.inString == True:
-                        self.inString = False
-                        self.token.append(self.lastToken)
-                        self.lastToken = ""
-                    else:
-                        self.inString = True
-                elif self.inString == True:
-                    if i == "'" or i == '"' or "'" in i or '"' in i:
-                        count = 0
-                        for j in range(len(i)):
-                            tmp = j
-                            if tmp == "'" and i[j+1] != "'" and j+1<len(i):
-                                count += 1
-                            elif tmp !="'" and i[j+1] == "'" and j+1<len(i):
-                                count += 1
-                            elif tmp != "'" and i[j+1] == "'" and j+1<len(i):
-                                count += 0
-                            elif tmp == "'" and  j+1>len(i):
-                                count += 1
-                        if (count % 2) == 0:
-                            self.inString = False
-                        else:
-                            self.inString = True
-                    else:
-                        pass
-                elif self.inString == False:
-                    self.token.append(self.lastToken)
-                    self.lastToken = ""
-            elif i != "":
+            if i != "" and ("'" not in i and '"' not in i) and (self.inString % 2) == 1:
                 self.token.append(self.lastToken)
                 self.lastToken = ""
-            else:
-                pass
+            elif  i != "" and ("'" not in i and '"' not in i) and (self.inString % 2) == 0:
+                self.lastToken += " "
+            elif i != "" and ("'" in i or '"' in i):
+                self.inString += 1
+                self.lastToken += " "
+        self.lastToken = self.token[len(self.token)-1]
+        self.token.pop()
+        self.tmpToken = self.lastToken[(len(self.lastToken)-1):]
+        self.lastToken = self.lastToken[:(len(self.lastToken)-2)]
+        self.token.append(self.lastToken)
+        self.token.append(self.tmpToken)
         for n in range(len(self.token)):
             i = self.token[n]
             if i in self.keywords:
                 if i != "=" and i != ";":
-                    self.token[n] = self.keywords[i] + "::" + i
+                    self.token[n] = self.keywords[i]
                 else:
                     self.token[n] = self.keywords[i]
                 if i == "new":
@@ -122,6 +101,8 @@ class Lexer:
                         pass
             elif re.search(r"[0-9]",i):
                 self.token[n] = "INT" + "::" + i
+            elif re.search(r"[0-9.]",i):
+                self.token[n] = "FLT" + "::" + i
             elif re.search(r"[0-9a-zA-Z_-]", i):
                 self.token[n] = "STR" + "::" + i
             else:
@@ -130,4 +111,4 @@ class Lexer:
 lexer = Lexer()
 
 #print(lexer.tokenize(input("Enter Code : ")))
-print(lexer.tokenize('new a = "james";'))
+print(lexer.tokenize(input("Enter Code : ")))
